59f69e3e8029a2c1f741d149da898e4cab76ee7b
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-19 16:28:54 -0700

    limited iterations

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@
 
 // array to hold all of the Product objects
 Product.allProducts = [];
+Product.chosenProducts = [];
+var numOfSelections = 0;
 
 // make a Product constructor for product objects
 function Product(filepath, name) {
   this.filepath = filepath; // the right-hand side needs to match the parameter, but the left side can be whatever you want.
   this.name = name;
+  this.timesSelected = 0;
+  this.timesShown = 0;
   Product.allProducts.push(this);
 }
 
@@ -32,22 +36,53 @@ new Product('image/usb.gif', 'USB');
 new Product('image/water-can.jpg', 'Water Can');
 new Product('image/wine-glass.jpg', 'Wine Glass');
 
-// access the element from the DOM
-var imgElement = document.getElementById('product-pic');
+// access the elements from the DOM
+var imgElement1 = document.getElementById('product-pic1');
+var imgElement2 = document.getElementById('product-pic2');
+var imgElement3 = document.getElementById('product-pic3');
 
-// add an event listener
-imgElement.addEventListener('click', randomProduct);
+Product.chosenProducts.push(imgElement1);
+Product.chosenProducts.push(imgElement2);
+Product.chosenProducts.push(imgElement3);
+
+// add an event listener for each element
+/*
+imgElement1.addEventListener('click', randomProduct);
+imgElement2.addEventListener('click', randomProduct);
+imgElement3.addEventListener('click', randomProduct);
+*/
+
+for (var i = 0; i < Product.chosenProducts.length; i++) {
+  Product.chosenProducts[i].addEventListener('click', randomProduct);
+}
+console.log(Product.chosenProducts[0]);
 
 // callback function when image is clicked:
 function randomProduct() {
-  // random number generator
-  var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
+  if (numOfSelections < 5) {
+    var randomIndexArray = [];
 
-  // for loop to iterate over the array and render one product
-  imgElement.src = Product.allProducts[randomIndex].filepath;
-  imgElement.alt = Product.allProducts[randomIndex].name;
 
+    // random number generator
+    var randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
+    var randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
+    var randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
+    randomIndexArray.push(randomIndex1);
+    randomIndexArray.push(randomIndex2);
+    randomIndexArray.push(randomIndex3);
+
+    // for loop to iterate over the array and render one product
+    for (var i = 0; i < Product.chosenProducts.length; i++) {
+      Product.chosenProducts[i].src = Product.allProducts[randomIndexArray[i]].filepath;
+      Product.chosenProducts[i].alt = Product.allProducts[randomIndexArray[i]].name;
+    }
+    numOfSelections++;
+  }
+  else {
+    alert('All done!');
+  }
 }
+
 
 // render an image on page load
 randomProduct();
