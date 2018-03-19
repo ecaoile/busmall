8d174f5f2bfdf8994e1b19c7213a422dc0bae5f4
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-19 14:48:56 -0700

    spell check on file names

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // array to hold all of the Product objects
-var allProducts = [];
+Product.allProducts = [];
 
 // make a Product constructor for product objects
 function Product(filepath, name) {
   this.filepath = filepath; // the right-hand side needs to match the parameter, but the left side can be whatever you want.
   this.name = name;
-  allProducts.push(this);
+  Product.allProducts.push(this);
 }
 
 // new instances of Product
@@ -18,17 +18,17 @@ new Product('image/boots.jpg', 'Boots');
 new Product('image/breakfast.jpg', 'Breakfast');
 new Product('image/bubblegum.jpg', 'Bubblegum');
 new Product('image/chair.jpg', 'Chair');
-new Product('image/Cthulu.jpg', 'Cthulu');
+new Product('image/cthulhu.jpg', 'Cthulu');
 new Product('image/dog-duck.jpg', 'Dog Duck');
 new Product('image/dragon.jpg', 'Dragon');
 new Product('image/pen.jpg', 'Pen');
 new Product('image/pet-sweep.jpg', 'Pet Sweep');
 new Product('image/scissors.jpg', 'Scissors');
 new Product('image/shark.jpg', 'Shark');
-new Product('image/sweep.jpg', 'Sweep');
+new Product('image/sweep.png', 'Sweep');
 new Product('image/tauntaun.jpg', 'Tauntaun');
 new Product('image/unicorn.jpg', 'Unicorn');
-new Product('image/usb.jpg', 'USB');
+new Product('image/usb.gif', 'USB');
 new Product('image/water-can.jpg', 'Water Can');
 new Product('image/wine-glass.jpg', 'Wine Glass');
 
@@ -36,18 +36,18 @@ new Product('image/wine-glass.jpg', 'Wine Glass');
 var imgElement = document.getElementById('product-pic');
 
 // add an event listener
-imgElement.addEventListener('click', randomGoat);
+imgElement.addEventListener('click', randomProduct);
 
 // callback function when image is clicked:
-function randomGoat() {
+function randomProduct() {
   // random number generator
   var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
 
-  // for loop to iterate over the array and render one goat
+  // for loop to iterate over the array and render one product
   imgElement.src = Product.allProducts[randomIndex].filepath;
   imgElement.alt = Product.allProducts[randomIndex].name;
 
 }
 
 // render an image on page load
-randomGoat();
+randomProduct();
