9bc6a028c823fdf03e99f4f6303457097184a159
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-20 14:56:13 -0700

    passes javascript functionality requirements

--- js/app.js
+++ js/app.js
@@ -5,13 +5,29 @@ Product.allProducts = [];
 Product.lastDisplayed = [];
 Product.numOfVotes = 0;
 
+var productNames = [];
+
+//var productVotes = [];
+
+// access the img elements from the DOM
+var imgElement1 = document.getElementById('product-pic1');
+var imgElement2 = document.getElementById('product-pic2');
+var imgElement3 = document.getElementById('product-pic3');
+
+// access the section element from the DOM
+var sectionElement = document.getElementById('three-pics');
+
+// access the unordered list element from the DOM
+var unorderedListElement = document.getElementById('results');
+
 // make a Product constructor for product objects
 function Product(filepath, name) {
-  this.filepath = filepath; // the right-hand side needs to match the parameter, but the left side can be whatever you want.
+  this.filepath = filepath;
   this.name = name;
+  this.timesDisaplyed = 0;
   this.timesSelected = 0;
-  this.times = 0;
   Product.allProducts.push(this);
+  productNames.push(this.name);
 }
 
 // new instances of Product
@@ -36,99 +52,60 @@ new Product('image/usb.gif', 'USB');
 new Product('image/water-can.jpg', 'Water Can');
 new Product('image/wine-glass.jpg', 'Wine Glass');
 
-
-// access the elements from the DOM
-var imgElement1 = document.getElementById('product-pic1');
-var imgElement2 = document.getElementById('product-pic2');
-var imgElement3 = document.getElementById('product-pic3');
-var sectionElement = document.getElementById('three-pics');
-Product.lastDisplayed.push(imgElement1);
-Product.lastDisplayed.push(imgElement2);
-Product.lastDisplayed.push(imgElement3);
-
-// add an event listener for each element
-imgElement1.addEventListener('click', randomProduct);
-imgElement2.addEventListener('click', randomProduct);
-imgElement3.addEventListener('click', randomProduct);
-sectionElement.addEventListener('click', randomProduct);
-
-/*
-imgElement1.addEventListener('click', chosePic1());
-imgElement2.addEventListener('click', chosePic2());
-imgElement3.addEventListener('click', chosePic3());
-*/
-
-/*
-for (var i = 0; i < Product.chosenProducts.length; i++) {
-  Product.chosenProducts[i].addEventListener('click', randomProduct);
-  Product.chosenProducts[i].addEventListener('click', addTimesShown);
-i =0 ; i < array.length ; i++
-  event.target.id === array[i].name
-  this.votecounter ++
-
-
-}*/
-
-
 // callback function when image is clicked:
 function randomProduct() {
-
-  /*
-  if (numOfVotes > 0) {
-    for (var i = 0; i < Product.allProducts.length; i++) {
-      if (event.target.alt === Product.allProducts[i].name) {
-        Product.allProducts[i].timesSelected++;
-
-        console.log(event.target.alt);
-        console.log(Product.allProducts[i].timesSelected);
-
-      }
-    }
-  }*/
-
   // random number generator
-  if (Product.numOfVotes < 4) {
-    var randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
-    var randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
-    var randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
+  var randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
+  var randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
+  var randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
 
-    // prevent duplicates
-    while (randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3 || Product.lastDisplayed.includes(Product.allProducts[randomIndex1]) || Product.lastDisplayed.includes(Product.allProducts[randomIndex2]) || Product.lastDisplayed.includes(Product.allProducts[randomIndex3])) {
-      randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
-      randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
-      randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
-    }
-    imgElement1.src = Product.allProducts[randomIndex1].filepath;
-    imgElement1.alt = Product.allProducts[randomIndex1].name;
-    imgElement2.src = Product.allProducts[randomIndex2].filepath;
-    imgElement2.alt = Product.allProducts[randomIndex2].name;
-    imgElement3.src = Product.allProducts[randomIndex3].filepath;
-    imgElement3.alt = Product.allProducts[randomIndex3].name;
-
-    Product.lastDisplayed.splice(0, Product.allProducts[randomIndex1]);
-    Product.lastDisplayed.splice(1, Product.allProducts[randomIndex2]);
-    Product.lastDisplayed.splice(2, Product.allProducts[randomIndex3]);
-    //console.log(numOfVotes);
-    //console.log(Product.lastDisplayed);
+  // prevent duplicates
+  while (randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3 || Product.lastDisplayed.includes(randomIndex1) || Product.lastDisplayed.includes(randomIndex2) || Product.lastDisplayed.includes(randomIndex3)) {
+    console.log('Duplicate caught!');
+    console.log(Product.lastDisplayed);
+    randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
+    randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
+    randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
+    //debugger;
   }
+
+  // display the 3 unique images on the screen
+  imgElement1.src = Product.allProducts[randomIndex1].filepath;
+  imgElement1.alt = Product.allProducts[randomIndex1].name;
+
+  imgElement2.src = Product.allProducts[randomIndex2].filepath;
+  imgElement2.alt = Product.allProducts[randomIndex2].name;
+
+  imgElement3.src = Product.allProducts[randomIndex3].filepath;
+  imgElement3.alt = Product.allProducts[randomIndex3].name;
+
+  // place index values of pictures into the lastDisplayed array
+  Product.lastDisplayed[0] = randomIndex1;
+  Product.lastDisplayed[1] = randomIndex2;
+  Product.lastDisplayed[2] = randomIndex3;
+
+  // increment the numbers of times displayed
+  Product.allProducts[randomIndex1].timesDisaplyed++;
+  Product.allProducts[randomIndex2].timesDisaplyed++;
+  Product.allProducts[randomIndex3].timesDisaplyed++;
 }
 
 function handleClick(event) {
-  //increment click counter
+  // increment click counter
   Product.numOfVotes++;
   console.log(Product.numOfVotes);
-  //increment clicks/votes on the specific image
+  // increment clicks/votes on the specific image
   for (var i in Product.allProducts) {
     if (event.target.alt === Product.allProducts[i].name) {
       Product.allProducts[i].timesSelected++;
-      console.log(event.target.alt);
-      console.log(Product.allProducts[i].timesSelected);
-
     }
   }
 
   // check the click counter
-  if (Product.numOfVotes > 4) {
+  if (Product.numOfVotes > 24) {
+    sectionElement.removeEventListener('click', handleClick);
+
+    // after 25 clicks, display results as a list
     showResults();
   }
   else {
@@ -137,10 +114,15 @@ function handleClick(event) {
 }
 
 function showResults() {
-  for (var i = 0; i < Product.allProducts.length; i++) {
-    console.log(Product.allProducts[i].name + ': ' + Product.allProducts[i].timesSelected);
-    //console.log(numOfVotes);
+  for (var i in Product.allProducts) {
+    // 1. target/create the element (li);
+    var listItemElement = document.createElement('li');
 
+    // 2. give it content
+    listItemElement.textContent = Product.allProducts[i].name + ' has ' + Product.allProducts[i].timesSelected + ' votes and was displayed ' + Product.allProducts[i].timesDisaplyed + ' times.';
+
+    // 3. append the elemenet to its parent
+    unorderedListElement.appendChild(listItemElement);
   }
   // create list items to display the number of times each goat was displayed and the numbe of votes each one received
   // 1. target/create the element
@@ -148,11 +130,21 @@ function showResults() {
 }
 
 sectionElement.addEventListener('click', handleClick);
-/*
-imgElement1.addEventListener('click', handleClick);
-imgElement2.addEventListener('click', handleClick);
-imgElement3.addEventListener('click', handleClick);
-*/
 
 // render an image on page load
 randomProduct();
+
+/*
+function renderChart() {
+  var context = document.getElementById('product-chart').getContext('2d');
+
+  var goatChart = newChart(context, {
+    type: bar,
+    data: [{
+      labels: 'Votes per Product',
+      data: productVotes,
+      backgroundColor: arrayOfColors,
+    }]
+  },
+)
+}*/
