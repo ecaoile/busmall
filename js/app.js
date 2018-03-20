fa22dd083a73d7ba4a3399dbbed6bebefccd5b6d
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-20 11:23:38 -0700

    got javascript functionality working...I think

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 // array to hold all of the Product objects
 Product.allProducts = [];
-Product.chosenProducts = [];
-var numOfVotes = 0;
+Product.lastDisplayed = [];
+Product.numOfVotes = 0;
 
 // make a Product constructor for product objects
 function Product(filepath, name) {
   this.filepath = filepath; // the right-hand side needs to match the parameter, but the left side can be whatever you want.
   this.name = name;
   this.timesSelected = 0;
-  this.timesShown = 0;
+  this.times = 0;
   Product.allProducts.push(this);
 }
 
@@ -41,16 +41,16 @@ new Product('image/wine-glass.jpg', 'Wine Glass');
 var imgElement1 = document.getElementById('product-pic1');
 var imgElement2 = document.getElementById('product-pic2');
 var imgElement3 = document.getElementById('product-pic3');
-
-Product.chosenProducts.push(imgElement1);
-Product.chosenProducts.push(imgElement2);
-Product.chosenProducts.push(imgElement3);
+var sectionElement = document.getElementById('three-pics');
+Product.lastDisplayed.push(imgElement1);
+Product.lastDisplayed.push(imgElement2);
+Product.lastDisplayed.push(imgElement3);
 
 // add an event listener for each element
-
 imgElement1.addEventListener('click', randomProduct);
 imgElement2.addEventListener('click', randomProduct);
 imgElement3.addEventListener('click', randomProduct);
+sectionElement.addEventListener('click', randomProduct);
 
 /*
 imgElement1.addEventListener('click', chosePic1());
@@ -71,25 +71,29 @@ i =0 ; i < array.length ; i++
 
 
 // callback function when image is clicked:
-function randomProduct(event) {
-  if (numOfVotes < 25) {
-    if (numOfVotes > 0) {
-      for (var i = 0; i < Product.allProducts.length; i++) {
-        if (event.target.alt === Product.allProducts[i].name) {
-          Product.allProducts[i].timesSelected++;
-          console.log(event.target.alt);
-          console.log(Product.allProducts[i].timesSelected);
-        }
+function randomProduct() {
+
+  /*
+  if (numOfVotes > 0) {
+    for (var i = 0; i < Product.allProducts.length; i++) {
+      if (event.target.alt === Product.allProducts[i].name) {
+        Product.allProducts[i].timesSelected++;
+
+        console.log(event.target.alt);
+        console.log(Product.allProducts[i].timesSelected);
+
       }
     }
-    // random number generator
+  }*/
 
+  // random number generator
+  if (Product.numOfVotes < 4) {
     var randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
     var randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
     var randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
 
     // prevent duplicates
-    while (randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3) {
+    while (randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3 || Product.lastDisplayed.includes(Product.allProducts[randomIndex1]) || Product.lastDisplayed.includes(Product.allProducts[randomIndex2]) || Product.lastDisplayed.includes(Product.allProducts[randomIndex3])) {
       randomIndex1 = Math.floor(Math.random() * Product.allProducts.length);
       randomIndex2 = Math.floor(Math.random() * Product.allProducts.length);
       randomIndex3 = Math.floor(Math.random() * Product.allProducts.length);
@@ -101,29 +105,54 @@ function randomProduct(event) {
     imgElement3.src = Product.allProducts[randomIndex3].filepath;
     imgElement3.alt = Product.allProducts[randomIndex3].name;
 
-    Product.chosenProducts.push(Product.allProducts[randomIndex1]);
-    Product.chosenProducts.push(Product.allProducts[randomIndex2]);
-    Product.chosenProducts.push(Product.allProducts[randomIndex3]);
-    numOfVotes++;
-    console.log(numOfVotes);
-    console.log(Product.chosenProducts);
+    Product.lastDisplayed.splice(0, Product.allProducts[randomIndex1]);
+    Product.lastDisplayed.splice(1, Product.allProducts[randomIndex2]);
+    Product.lastDisplayed.splice(2, Product.allProducts[randomIndex3]);
+    //console.log(numOfVotes);
+    //console.log(Product.lastDisplayed);
   }
-
-  //test
-
-
-
-
-  //var randomIndexArray = [randomIndex1, randomIndex2, randomIndex3];
-  // for loop to iterate over the array and render three products
-  /*
-    for (var i = 0; i < Product.chosenProducts.length; i++) {
-      Product.chosenProducts[i].src = Product.allProducts[randomIndexArray[i]].filepath;
-      Product.chosenProducts[i].alt = Product.allProducts[randomIndexArray[i]].name;
-    }*/
 }
 
+function handleClick(event) {
+  //increment click counter
+  Product.numOfVotes++;
+  console.log(Product.numOfVotes);
+  //increment clicks/votes on the specific image
+  for (var i in Product.allProducts) {
+    if (event.target.alt === Product.allProducts[i].name) {
+      Product.allProducts[i].timesSelected++;
+      console.log(event.target.alt);
+      console.log(Product.allProducts[i].timesSelected);
 
+    }
+  }
+
+  // check the click counter
+  if (Product.numOfVotes > 4) {
+    showResults();
+  }
+  else {
+    randomProduct();
+  }
+}
+
+function showResults() {
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    console.log(Product.allProducts[i].name + ': ' + Product.allProducts[i].timesSelected);
+    //console.log(numOfVotes);
+
+  }
+  // create list items to display the number of times each goat was displayed and the numbe of votes each one received
+  // 1. target/create the element
+  // 2. give it content
+}
+
+sectionElement.addEventListener('click', handleClick);
+/*
+imgElement1.addEventListener('click', handleClick);
+imgElement2.addEventListener('click', handleClick);
+imgElement3.addEventListener('click', handleClick);
+*/
 
 // render an image on page load
 randomProduct();
